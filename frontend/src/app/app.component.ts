853176e40c9487a5a52ce739dbf084b4898acf7f
Anthony G <tonixoskv7@gmail.com>
2025-01-09 13:48:35 -0500

    actualizacion de rutas

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,23 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, Router } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
-
-  constructor(private router: Router) {
-    goToPozos() {
-      this.router.navigate(['pozos']);
-    }
-
-    goToForm() {
-      this.router.navigate(['form']);
-    }
-  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { ModifyViewComponent } from './modify-view/modify-view.component';
 import { FormViewComponent } from './form-view/form-view.component';
 
 export const routes: Routes = [
-    { path: 'pozos', component: ModifyViewComponent },
-    { path: 'form', component: FormViewComponent },
+    {
+        path: 'pozos',
+        component: ModifyViewComponent
+    },
+    {
+        path: 'form',
+        component: FormViewComponent
+    },
 ];
