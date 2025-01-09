6ead6ced3e48495ebfd6ad4123c4ceed9b29600f
Anthony G <tonixoskv7@gmail.com>
2025-01-09 09:48:42 -0500

    Creacion de componente vista-modificacion

---
+++ frontend/src/app/modify-view/modify-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './modify-view.component.html',
+  styleUrl: './modify-view.component.css'
+})
+export class ModifyViewComponent {
+
+}
