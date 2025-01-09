d7fd5134ac9fc63f14386ef669a15a6c5b1d71b9
Anthony G <tonixoskv7@gmail.com>
2025-01-09 15:30:07 -0500

    Implementacion vista-formulario

--- frontend/src/app/form-view/form-view.component.ts
+++ frontend/src/app/form-view/form-view.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-form-view',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './form-view.component.html',
   styleUrl: './form-view.component.css'
 })
 export class FormViewComponent {
-
+  constructor(private route: ActivatedRoute) { }
 }
